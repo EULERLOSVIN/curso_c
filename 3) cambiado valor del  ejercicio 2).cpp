209db0f2ee6c43128c042795b4842ec30efef6ee
Euler Losvin <eulerlosvinmartinezhurtado@gamil.com>
2023-04-16 19:55:52 -0500

    actualizando mi trabajo

--- 3) cambiado valor del  ejercicio 2).cpp
+++ 3) cambiado valor del  ejercicio 2).cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-    system("cls");
+    system("cls");//limpiar pantalla
     
     
-    int num = 5, i;      // limpiar pantalla
+    int num = 5, i;      
     int *p;   //declarando la variable que representa al puntero
     
     p = &num; // muestra la direccion de memoria de la variable
@@ -17,12 +17,12 @@ int main()
     else
     {
         i=2;          //i es el divisor
-        while(num%i!=0) //mientras el número sea divisible por el divisor
+        while(num%i!=0) //mientras el numero sea divisible por el divisor
         {
             i++;      //siguiente divisor
         }
-        //cuando sale del while, si el divisor es el propio número
-        //entonces el número es primo
+        //cuando sale del while, si el divisor es el propio numero
+        //entonces el numero es primo
         if(i==num)      
             cout << "\nel numero es Primo" << endl;   
         else
