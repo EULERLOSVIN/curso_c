b9765355fc12f3019ec4106d52cc2be449050b3c
Euler Losvin <eulerlosvinmartinezhurtado@gamil.com>
2023-05-22 18:03:28 -0500

    20/05/2023

---
+++ trajado en equipo en clases/notas.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <fstream> //libreira para archivos
+// #include "facultad_01.cpp"
+#include "estudiante.cpp"
+
+using namespace std;
+int main(){
+
+    Facultad facu1("001", "Facultad de Ingenieria en Informatica y Sistemas", "FIIS");
+    Facultad facu2("002", "Facultad de de Zootecnia", "FZ");
+   
+   // Estudiantes de FIIS
+    Estudiante est1("2023000001", "juanito.perez@unas.edu.pe", "perez", "juanito", "44444444");
+    est1.setFacultad(facu1);
+    Estudiante est2("20220456", "jose.caldas@unas.edu.pe", " jose", "caldas", "234567");
+    est2.setFacultad(facu1);
+    Estudiante est3("20223457", "carlos.solar@unas.edu.pe", " carlos", "solar", "345678");
+    est3.setFacultad(facu1);
+    Estudiante est4("20223412", "marco.palacios@unas.edu.pe", "marco", "palacios", "3457177");
+    est4.setFacultad(facu1);
+    // Estudiantes de ZOOTECNIA
+    Estudiante est5("2020007", "manuel.lopez@unas.edu.pe", "lopez", "manuel", "20458771");
+    est5.setFacultad(facu2);
+    Estudiante est6("2000485", "therry.exaltacion@unas.edu.pe", "exaltacion", "therry", "2024575");
+    est6.setFacultad(facu2);
+    Estudiante est7("20220050", "krystal.exaltacion@unas.edu.pe", "exaltacion", "krystal", "87541558");
+    est7.setFacultad(facu2);
+    Estudiante est8("2022002", "jose.martinez@unas.edu.pe", "mantinez", "jose", "73463283");
+    est8.setFacultad(facu2);
+
+
+
+
+
+    return 0;
+}
